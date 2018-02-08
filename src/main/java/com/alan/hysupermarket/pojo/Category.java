5f9d34f008f1cab399cb17993c5e6a24fd01b56e
Alan.Lan <lanhonghui100@gmail.com>
2018-02-08 21:48:20 +0800

    完成了Category业务服务实现类

--- src/main/java/com/alan/hysupermarket/pojo/Category.java
+++ src/main/java/com/alan/hysupermarket/pojo/Category.java
@@ -1,9 +1,32 @@
 package com.alan.hysupermarket.pojo;
 
+import java.util.List;
+
 public class Category {
     private Long ID;
 
     private String NAME;
+
+    /*如下是非数据库字段*/
+    private List<Product> products;
+
+    private List<List<Product>> productsByRow;
+
+    public List<Product> getProducts() {
+        return products;
+    }
+
+    public void setProducts(List<Product> products) {
+        this.products = products;
+    }
+
+    public List<List<Product>> getProductsByRow() {
+        return productsByRow;
+    }
+
+    public void setProductsByRow(List<List<Product>> productsByRow) {
+        this.productsByRow = productsByRow;
+    }
 
     public Long getID() {
         return ID;
--- src/main/java/com/alan/hysupermarket/service/ICategoryService.java
+++ src/main/java/com/alan/hysupermarket/service/ICategoryService.java
@@ -1,4 +1,19 @@
 package com.alan.hysupermarket.service;
 
+import com.alan.hysupermarket.pojo.Category;
+
+import java.util.List;
+
 public interface ICategoryService {
+
+    List<Category> list();
+
+    void add(Category category);
+
+    void delete(long id);
+
+    Category get(long id);
+
+    void update(Category category);
+
 }
