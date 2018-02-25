f81d0dfd97c4bb5f6537d236670228a1038107e4
Alan.Lan <lanhonghui100@gmail.com>
2018-02-25 17:45:09 +0800

    完善了照片与分页工具类

--- src/main/java/com/alan/hysupermarket/utils/UploadedImageFile.java
+++ src/main/java/com/alan/hysupermarket/utils/UploadedImageFile.java
@@ -1,4 +1,20 @@
 package com.alan.hysupermarket.utils;
 
+import org.springframework.web.multipart.MultipartFile;
+
+/**
+ * 上传照片文件工具类
+ */
 public class UploadedImageFile {
+
+    MultipartFile image;
+
+    public MultipartFile getImage() {
+        return image;
+    }
+
+    public void setImage(MultipartFile image) {
+        this.image = image;
+    }
+
 }
